bee50bdfe75ec33f70bdb47d0f2d5792dd291b87
Skeby <103804946+Skeby@users.noreply.github.com>
2024-07-13 15:46:48 +0100

    feat: include redux-persist migration for stale storage data

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,14 +2,34 @@ import { configureStore } from "@reduxjs/toolkit"
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
 import appReducer, { AppState } from "./slices/appSlice"
 import storage from "redux-persist/lib/storage"
-import { persistStore, persistReducer } from "redux-persist"
+import {
+  persistStore,
+  persistReducer,
+  createMigrate,
+  MigrationManifest,
+} from "redux-persist"
 
 export interface PersistedAppState extends AppState {}
 
+const migrations: MigrationManifest = {
+  // Example migration to clear state when version changes
+  2: () => {
+    return {
+      _persist: {
+        version: 2, // Set the version to the new version
+        rehydrated: true, // Set rehydrated to true to indicate the state has been migrated
+      },
+    }
+  },
+  // Add more migrations as needed
+}
+
 const appPersistConfig = {
   key: "app",
+  version: 2,
   storage: storage,
   whitelist: ["favorites", "cart"],
+  migrate: createMigrate(migrations, { debug: false }),
 }
 
 export const store = configureStore({
